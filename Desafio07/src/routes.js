37f04550a52a1f28647c65c3b94109d53a1db65d
burn-c <chopcsom@gmail.com>
2019-11-17 16:47:34 -0300

    Config: ReactNavigation

---
+++ Desafio07/src/routes.js
@@ -0,0 +1,23 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Home,
+      Cart,
+    },
+    {
+      headerBackTitleVisible: false,
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#000',
+        },
+      },
+    }
+  )
+);
+
+export default Routes;
