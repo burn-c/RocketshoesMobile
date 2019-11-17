a2f9b10f9bf3e62fdd92a2546b2c5c6cf9bf6646
burn-c <chopcsom@gmail.com>
2019-11-17 18:58:35 -0300

    Feature: Header

--- Desafio07/src/routes.js
+++ Desafio07/src/routes.js
@@ -1,7 +1,10 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+
+import Header from './components/Header';
 
 const Routes = createAppContainer(
   createStackNavigator(
@@ -10,11 +13,12 @@ const Routes = createAppContainer(
       Cart,
     },
     {
-      headerBackTitleVisible: false,
-      defaultNavigationOptions: {
-        headerStyle: {
-          backgroundColor: '#000',
-        },
+      // initialRouteName: 'Cart',
+      defaultNavigationOptions: navigation => ({
+        header: <Header {...navigation} />,
+      }),
+      cardStyle: {
+        backgroundColor: '#000',
       },
     }
   )
