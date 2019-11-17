297b01f14d49041ee2cd82cfdee01c28ddbb8945
burn-c <chopcsom@gmail.com>
2019-11-17 20:13:41 -0300

    Styles: Home

--- Desafio07/src/pages/Home/index.js
+++ Desafio07/src/pages/Home/index.js
@@ -1,8 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Product,
+  ProdutImage,
+  ProductTitle,
+  ProductPrice,
+  AddButton,
+  ProductAmount,
+  ProductAmountText,
+  AddButtonText,
+} from './styles';
 
-export default function Home() {
-  return <View />;
+export default class Home extends Component {
+  render() {
+    return (
+      <>
+        <FlatList horizontal />
+        <Container>
+          <Product>
+            <ProdutImage />
+            <ProductTitle>Tênis de Caminhada Leve Confortável</ProductTitle>
+            <ProductPrice>R$300.00</ProductPrice>
+            <AddButton>
+              <ProductAmount>
+                <Icon name="add-shopping-cart" color="#FFF" size={20} />
+                <ProductAmountText>0</ProductAmountText>
+              </ProductAmount>
+              <AddButtonText>ADICIONAR</AddButtonText>
+            </AddButton>
+          </Product>
+        </Container>
+      </>
+    );
+  }
 }
